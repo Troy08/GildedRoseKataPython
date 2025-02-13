7c9c0f33b467a8c77f12eb75adc45d36b342b6a3
Weicheng Wang <troy54885@gmail.com>
2025-02-12 19:53:14 -0800

    Fixed test cases for Gilded Rose Kata

--- test_gilded_rose.py
+++ test_gilded_rose.py
@@ -10,8 +10,8 @@ class GildedRoseTest(unittest.TestCase):
         gilded_rose.update_quality()
         sulfuras_item = items[0]
 
-        self.assertEqual(79, sulfuras_item.quality)
-        self.assertEqual(4, sulfuras_item.sell_in)
+        self.assertEqual(80, sulfuras_item.quality)
+        self.assertEqual(5, sulfuras_item.sell_in)
 
     def test_aged_brie_exceeds_quality_limit(self):
         items = [Item("Aged Brie", 10, 50)]
@@ -19,7 +19,7 @@ class GildedRoseTest(unittest.TestCase):
         gilded_rose.update_quality()
         aged_brie = items[0]
 
-        self.assertGreater(aged_brie.quality, 50)
+        self.assertEqual(50, aged_brie.quality)
 
     def test_quality_decreases_twice_as_fast_after_sell_in(self):
         items = [Item("Normal Item", 0, 10)]
@@ -27,13 +27,13 @@ class GildedRoseTest(unittest.TestCase):
         gilded_rose.update_quality()
         normal_item = items[0]
 
-        self.assertEqual(7, normal_item.quality)
+        self.assertEqual(8, normal_item.quality)
 
     def test_gilded_rose_list_all_items(self):
         items = [Item("Sulfuras, Hand of Ragnaros", 5, 80)]
         gilded_rose = GildedRose(items)
 
-        all_items = gilded_rose.get_items()
+        all_items = [item.name for item in gilded_rose.items]
         self.assertIsNotNone(all_items)
 
 
